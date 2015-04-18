9cddc5dd79dc4a7606b043e325339ae9c7b3f9d8
dungnv53 <vandung_17@yahoo.com>
2015-04-18 10:02:37 +0700

    Add Batch, jap_vowel

---
+++ Code/Batch/create_bootable_usb.bat
@@ -0,0 +1,12 @@
+SELECT DISK <DRIVE NUMBER>
+CLEAN
+CREATE PARTITION PRIMARY
+SELECT PARTITION 1
+ACTIVE
+FORMAT FS=NTFS QUICK
+ASSIGN
+EXIT
+
+
+// need BOOT/BOOTSEC.EXE (i do not remember exactly) /NT60 F:
+// This is suck because if I run Win x64 n want create boot x86 --> bug
