c34734bcddbdcfc0d5fd4b7e939e1468c0e861f4
dungnv53 <vandung_17@yahoo.com>
2015-07-16 23:50:46 +0700

    add crop script

---
+++ Code/SH/Crop/crop_area.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+DATE=$(date +"%Y-%m-%d_%H_%M_%S")
+import ~/bin/crop/$DATE.png
+# -descend
---
+++ Code/SH/Crop/scrot.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+DATE=$(date +"%Y-%m-%d_%H_%M_%S")
+scrot -s ~/bin/crop/$DATE.png
+# -descend
