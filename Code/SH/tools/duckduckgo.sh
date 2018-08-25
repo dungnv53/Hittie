fee8691fc9c23fe2d7bf18609a46223f44d9abbb
Jimi Hendrix <vandung53cc@gmail.com>
2018-08-26 00:22:22 +0700

    Update w3m bash search with auto concat parameters as search query

--- Code/SH/tools/duckduckgo.sh
+++ Code/SH/tools/duckduckgo.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
-w3m https://www.google.com/search?q="$1"
+func(){
+    str=
+    for i in "$@"; do
+        str="$str$i+"
+    done
+    #echo ${str# }
+    #echo $str
+    w3m https://www.duckduckgo.com/search?q=${str# }
+}
+
+#func
+func $1 $2 $3 $4 $5 $6 $7 $8 $9
